8346dae538d875f10904f45a98cb694cd4a49877
Jacob Bury <jakeb15@comcast.net>
2018-11-07 10:50:53 -0500

    anything

---
+++ file.js
@@ -0,0 +1,10 @@
+
+function sayHello() {
+ console.log("Hello, World")
+}
+alert("Hello, World!")
+
+function showAlert(Hello, Browser) {
+  alert ("Hello, World!");
+  <button onclick"showAlert()">Show alert</button>
+}
