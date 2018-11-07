14d83ff7c9d5ba3d0b4d003ece00bc82e8919688
jbury15 <43784680+jbury15@users.noreply.github.com>
2018-11-07 12:59:41 -0500

    finished changes

--- file.js
+++ file.js
@@ -1,10 +1,18 @@
 
-function sayHello() {
- console.log("Hello, World");
-}
-alert("Hello, World!")
+function sayHello() { ;
+ console.log("Hello, World") ;
+} ;
 
-function showAlert(Hello, Browser) {
-  alert ("Hello, World!");
-  <button onclick"showAlert()">Show alert</button>
-}
+function Thomas() { ;
+  alert("Hello, World!") ;
+} ;
+
+function Kackper() { ;
+ var p = document.getElementByID("hello") ;
+ p.innerHTML = "Hello, World!" ;
+} ;
+
+function helloStranger() { ;
+ var name = prompt("Enter Name") ;
+ alert("Hello " + name) ;
+ ) ;
